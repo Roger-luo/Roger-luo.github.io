d2bcb17544994c7c1bf69827b277eb45f4c1e084
Roger-luo <rogerluo.rl18@gmail.com>
2024-06-05 13:23:53 -0400

    add vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,7 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -47,4 +48,10 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
